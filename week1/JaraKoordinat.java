461c8b5e85c1ce952ff6b007b4510a126cb1f678
swietanurjanah <swietanurjanah@gmail.com>
2022-03-24 07:42:28 +0700

    add math task

---
+++ week1/JaraKoordinat.java
@@ -0,0 +1,35 @@
+package week1;
+
+import java.util.Scanner;
+
+public class JaraKoordinat {
+    //1. Buat program java yang menghitung jarak antara dua koordinat (x1, y1) dan (x2, y2) yang memiliki rumus:
+    //√(x2-x1)²+(y2-y1)²
+    public static void main(String[] args) {
+        int x2,x1,y2,y1;
+        double rumus;
+
+        Scanner k=new Scanner(System.in);
+        soal("X1");
+        x1=input(k);
+        soal("Y1");
+        y1=input(k);
+        soal("x2");
+        x2=input(k);
+        soal("Y2");
+        y2=input(k);
+        rumus = (Math.sqrt((Math.pow(x2-x1,2))+(Math.pow(y2-y1,2))));
+        System.out.println("Jarak antara ("+x1+","+y1+")" +" dan " +"("+ x2+","+y2+")"+" = " + rumus);
+
+    }
+
+   public  static  String soal(String nilai){
+        String s ="Masukan nilai "+nilai+" :";
+        System.out.print(s);
+        return s;
+   }
+    public static int input(Scanner k){
+        int key =k.nextInt();
+        return key;
+    }
+}
---
+++ week1/MenghitungBunga.java
@@ -0,0 +1,40 @@
+package week1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MenghitungBunga {
+    //2. Buat program Java untuk menghitung jumlah uang beserta bunga.
+    //
+    //A) Dana awal 10 juta dengan suku bunga 0.5% per bulan. Berapa uang yang diterima setelah 4 tahun?
+    //B) Dana awal 15 juta dengan suku bunga 6% per tahun. Berapa uang yang diterima setelah 5 tahun?
+    //
+    //A = p * (1 + r/n)^tn,
+    //
+    //P = dana awal
+    //r = suku bunga
+    //n = berapa kali bunga diterapkan per periode waktu
+    //t = jumlah periode waktu yang berlalu
+    public static void main(String[] args) {
+
+        double A,p,r,n,t;
+
+        Scanner k=new Scanner(System.in);
+        System.out.print("Masukan dana awal : ");
+        p=input(k);
+        System.out.print("Masukan suku bunga: ");
+        r=input(k);
+        System.out.print("masukan bunga per periode: ");
+        n=input(k);
+        System.out.print("masukan jumlah periode: ");
+        t=input(k);
+        double tn=n*t;
+        A=p*Math.pow(1+r/n,tn);
+        System.out.println("jumlah uang beserta bunga adalah : "+A);
+
+    }
+    public static double input(Scanner k){
+        double key= k.nextDouble();
+        return key;
+    }
+}
